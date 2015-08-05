a859aa54ea7cf4bd35f161836164e0e4105de1e1
Henrik Raitasola <heka.fof@gmail.com>
2015-08-05 22:58:52 +0300

    Lisätty Iron Router -paketti, jonka avulla käyttäjät voivat tehdä omia chat-huoneita. Huoneessa näkyy vain kyseisen huoneen chat-viestit.

--- chat.js
+++ chat.js
@@ -1,21 +1,43 @@
-MessagesList = new Meteor.Collection('messages');
+ChatRooms = new Mongo.Collection('chatrooms');
+
+Router.configure({
+  layoutTemplate: 'main'
+});
+
+Router.route('/', {
+  template: 'home'
+});
+
+Router.route('/:roomId', {
+  name: 'chat',
+  template: 'chat',
+  data: function() {
+    var currentRoomId = this.params.roomId;
+    return { messages: ChatRooms.find({roomId: currentRoomId}) };
+  }
+});
 
 if (Meteor.isClient) {
 
   Template.chat.helpers({
-    messages: function () {
-      return MessagesList.find();
-    }
   });
 
   Template.chat.events({
-    'submit form': function (e) {
+    'submit form': function(e) {
       e.preventDefault();
+      console.log(this);
       var messageBody = e.target.messageBody.value;
-      MessagesList.insert({
+      ChatRooms.insert({
+        roomId: Router.current().params.roomId,
         body: messageBody
       });
       e.target.messageBody.value = '';
+    }
+  });
+
+  Template.home.events({
+    'click button': function() {
+      Router.go('chat', {roomId: Random.id(6)});
     }
   });
 }
@@ -23,5 +45,11 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+  });
+
+  Meteor.methods({
+    'removeChatRooms': function() {
+      return ChatRooms.remove({});
+    }
   });
 }
